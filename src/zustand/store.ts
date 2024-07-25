d76cfc1640265aa4802f2b96cc3f4e9425c3a813
Shamanth Kumar Parameshwar <shamanth.parameshwar@xcelenergy.com>
2024-07-25 18:26:44 -0500

    Add Zustand store

---
+++ src/zustand/store.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand'
+
+type State = {
+    firstName: string
+    lastName: string
+  }
+  
+  type Action = {
+    updateFirstName: (firstName: State['firstName']) => void
+    updateLastName: (lastName: State['lastName']) => void
+  }
+  
+  // Create your store, which includes both state and (optionally) actions
+  export const useAppStore = create<State & Action>((set) => ({
+    firstName: '',
+    lastName: '',
+    updateFirstName: (firstName) => set(() => ({ firstName: firstName })),
+    updateLastName: (lastName) => set(() => ({ lastName: lastName })),
+  }))
